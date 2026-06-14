579e51b26c1f3141201052507b90d327a285f877
Musrad <musradabdul007@gmail.com>
2026-06-14 20:34:03 +0530

    created signin.js

---
+++ scripts/signin.js
@@ -0,0 +1,49 @@
+const signinForm = document.getElementById("signinForm");
+
+signinForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  document.getElementById("loginEmailError").textContent = "";
+  document.getElementById("loginPasswordError").textContent = "";
+
+  let email = document.getElementById("loginEmail").value.trim();
+  let password = document.getElementById("loginPassword").value;
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  let valid = true;
+
+  if (!emailRegex.test(email)) {
+    document.getElementById("loginEmailError").textContent =
+      "Invalid email format";
+    valid = false;
+  }
+
+  if (password === "") {
+    document.getElementById("loginPasswordError").textContent =
+      "Password is required";
+    valid = false;
+  }
+
+  if (!valid) return;
+
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  if (!storedUser) {
+    alert("No account found");
+    return;
+  }
+
+  if (email !== storedUser.email) {
+    document.getElementById("loginEmailError").textContent = "Email not found";
+    return;
+  }
+
+  if (password !== storedUser.password) {
+    document.getElementById("loginPasswordError").textContent =
+      "Wrong password";
+    return;
+  }
+
+  alert("Login Successful");
+});
