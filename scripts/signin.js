a4d51704b544a8a3674c2babe2f091060a66fd27
Musrad <musradabdul007@gmail.com>
2026-06-14 21:32:00 +0530

    made some updations

--- scripts/signin.js
+++ scripts/signin.js
@@ -2,23 +2,19 @@ const signinForm = document.getElementById("signinForm");
 
 signinForm.addEventListener("submit", function (e) {
   e.preventDefault();
-
   document.getElementById("loginEmailError").textContent = "";
   document.getElementById("loginPasswordError").textContent = "";
 
   let email = document.getElementById("loginEmail").value.trim();
   let password = document.getElementById("loginPassword").value;
-
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   let valid = true;
-
   if (!emailRegex.test(email)) {
     document.getElementById("loginEmailError").textContent =
       "Invalid email format";
     valid = false;
   }
-
   if (password === "") {
     document.getElementById("loginPasswordError").textContent =
       "Password is required";
@@ -26,9 +22,7 @@ signinForm.addEventListener("submit", function (e) {
   }
 
   if (!valid) return;
-
   const storedUser = JSON.parse(localStorage.getItem("user"));
-
   if (!storedUser) {
     alert("No account found");
     return;
@@ -44,10 +38,21 @@ signinForm.addEventListener("submit", function (e) {
       "Wrong password";
     return;
   }
-
   alert("Login Successful");
-
   localStorage.setItem("loggedIn", "true");
-
   window.location.href = "landingpage.html";
+});
+
+document.querySelectorAll(".toggle-eye").forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const input = icon.previousElementSibling;
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+  });
 });
--- scripts/signup.js
+++ scripts/signup.js
@@ -71,3 +71,17 @@ signupForm.addEventListener("submit", function (e) {
     window.location.href = "signin.html";
   }
 });
+
+document.querySelectorAll(".toggle-eye").forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const input = icon.previousElementSibling;
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.replace("fa-eye", "fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.replace("fa-eye-slash", "fa-eye");
+    }
+  });
+});
